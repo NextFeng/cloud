50537e66331db17d758c198c1bf21c90a93f7103
LinFeng <1225842878@qq.com>
2019-12-11 00:36:24 +0800

    v1.0

---
+++ lc-eureka-server/src/test/java/com/lcdz/cloud/eureka/LcEurekaServerApplicationTests.java
@@ -0,0 +1,8 @@
+package com.lcdz.cloud.eureka;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LcEurekaServerApplicationTests {
+
+}
---
+++ user/src/test/java/com/lcdz/cloud/user/UserApplicationTests.java
@@ -0,0 +1,8 @@
+package com.lcdz.cloud.user;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserApplicationTests {
+
+}
